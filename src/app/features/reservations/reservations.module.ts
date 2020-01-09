fb85dc330f788db5359c29f94899cd94e5dabdd5
scael85 <elisa.scambelloni@gmailcom>
2020-01-09 11:02:28 +0100

    versione con la home opaca

--- src/app/features/reservations/reservations.module.ts
+++ src/app/features/reservations/reservations.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import {FeatureReservationsRoutingModule} from './reservations.routing';
-import {ReservationAddComponent} from './add/reservation-add.component';
+import { FeatureReservationsRoutingModule } from './reservations.routing';
+import { ReservationAddComponent } from './add/reservation-add.component';
 import { ReservationListComponent } from './list/reservation-list.component';
 import { ReservationUpdComponent } from './upd/reservation-upd.component';
-import {ReservationService} from '../../shared/services/reservations.service';
-import {BeachService} from '../../shared/services/beaches.service';
-import {CoreModule} from '../../core/core.module';
-import {SortService} from '../../shared/services/sort.service';
+import { ReservationService } from '../../shared/services/reservations.service';
+import { BeachService } from '../../shared/services/beaches.service';
+import { CoreModule } from '../../core/core.module';
+import { SortService } from '../../shared/services/sort.service';
 import { ReservationDetailComponent } from './detail/reservation-detail.component';
 
 @NgModule({
